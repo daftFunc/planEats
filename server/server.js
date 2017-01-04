9b3ea1e30aabb1e665a21c45136a3b0a20427224
TreTuna <tretuna@gmail.com>
2017-01-04 14:10:52 -0500

    minor updates to server

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(cors());
 app.use(morgan('dev'));
 app.use(parser.json());
-app.use(express.static('../client/build'))
+app.use(express.static('../client/build'));
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`);
