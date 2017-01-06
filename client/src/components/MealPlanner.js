ea0b738069e7c072cdb3a50a09f18304f3e0b9f8
Brittany Starr <lunarunar@gmail.com>
2017-01-05 20:01:32 -0700

    basic functionality to add recipes to a meal

--- client/src/components/MealPlanner.js
+++ client/src/components/MealPlanner.js
@@ -12,6 +12,7 @@ class MealPlanner extends Component {
     super();
     this.state = {
       meals: meals,
+      mealName: null,
       recipes: recipes,
       clicked: [],
       recipeName: '',
@@ -23,32 +24,79 @@ class MealPlanner extends Component {
   }
 
   handleChange(meal) {
-    // console.log(meal)
-    this.state.clicked.push(meal)
+    //value of the text box
+    if (meal.target) {
+      this.setState({
+        mealName: meal.target.value
+      })
+    } else {
+      //item to add to array
+      var update = this.state.clicked.slice()
+      update.push(meal)
 
-    // this.setState({
-    //   clicked: this.state.clicked.push(meal)
-    // })
+      this.setState({
+        clicked: update
+      })
+    }
   }
 
-  handleSubmit() {
+  handleRemove(item) {
+
+    var update = this.state.clicked.slice()
+    update.splice(item, 1)
+
+    this.setState({
+      clicked: update
+    }, function() {
+      this.forceUpdate()
+    })
+
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
     /*on submit, data needs to be updated so that it renders in the recipe book (send to recipes array)*/
+    var newMeal = {
+      id: null,
+      name: this.state.mealName,
+      recipes: this.state.clicked
+    };
+
+    var updatedMeals = meals.concat(newMeal);
+
+    this.setState({
+      meals: updatedMeals,
+      mealName: ''
+    }, function(){
+      this.forceUpdate();
+    });
   }
 
   render() {
     return (
       <div className="container">
         <div className="recipes">
-          <h1>Add Recipe to New Meal</h1>
+          <h3>Click to add a recipe to your new meal</h3>
           <ul className="recipeList">
             {this.state.recipes.map((recipe, i) => {return <li className="recipeItem" id={i} onClick={this.handleChange.bind(this, recipe.title)}>{recipe.title}</li>})}
           </ul>
         </div>
         <div className="addNew">
-          <h1>New Meal</h1>
-          <ul>
-            <li>{this.state.clicked}</li>
-          </ul>
+          <form>
+            <FormControl
+              id="recipeName"
+              type="text"
+              value={this.state.mealName}
+              onChange={this.handleChange.bind(this)}
+              placeholder="Meal Name"
+            />
+            <ul>
+              {this.state.clicked.map((val, i) => {
+                return <li className="mealItems" id={i} onClick={this.handleRemove.bind(this, i)}>{val}</li>
+              })}
+            </ul>
+            <Button type="submit" onClick={this.handleSubmit.bind(this)}>Add Meal</Button>
+          </form>
         </div>
       </div>
     )
