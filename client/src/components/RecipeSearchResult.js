31b60ca7a67cf33b0e985978bed30f3deb53f652
TreTuna <tretuna@gmail.com>
2017-01-12 15:54:26 -0500

    fixed sweet alert in recipe search

--- client/src/components/RecipeSearchResult.js
+++ client/src/components/RecipeSearchResult.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {connectProfile} from '../auth';
 import swal from 'sweetalert2';
-import SweetAlert from 'sweetalert-react';
 import {Link} from 'react-router';
 import './RecipeSearchResult.css';
 import { /*FieldGroup, FormGroup, HelpBlock, ControlLabel, FormControl, */Button/*, Checkbox, Radio*/} from 'react-bootstrap';
@@ -15,6 +14,7 @@ class RecipeSearchResult extends Component {
     super(props);
     this.state = {
       username: JSON.parse(localStorage.profile).email,
+      show: false,
       recipe: this.props.recipe
     }
   }
@@ -41,30 +41,46 @@ class RecipeSearchResult extends Component {
       name: this.state.recipe.title,
       recipe: this.state.recipe
     });
+    swal(
+      'Added to your recipe book!',
+      'We can taste it already. Yum!'
+    );
     event.preventDefault();
   };
 
   displayFullRecipe(event){
+    let user = this.state.username;
     let recipe = this.state.recipe;
+    let ingredients = '<ol>';
+    for(let i = 0; i < recipe.extendedIngredients.length; i++){
+      ingredients = ingredients.concat(`<li>${recipe.extendedIngredients[i].originalString}</li>`);
+    }
+    ingredients = ingredients.concat('</ol>');
+    console.log(ingredients);
     swal({
       imageUrl: `https://spoonacular.com/recipeImages/${recipe.id}-312x231.jpg`,
       imageWidth: 312,
       imageHeight: 231,
       title: recipe.title,
-      text: 'Modal with a custom image.',
+      html: ingredients,
+      text: recipe.instructions,
       confirmButtonText: 'Add To Recipe Book',
       showCancelButton: true,
       cancelButtonText: 'Nah, not this one',
-      animation: false,
+      animation: true,
       customClass: 'animated bounceIn'
     })
-      .then(() => {
-        axios.post('/api/recipe', {
-          username: this.state.username,
-          name: this.state.recipe.title,
-          recipe: this.state.recipe
-        });
+    .then(() => {
+      axios.post('/api/recipe', {
+        username: user,
+        name: recipe.title,
+        recipe: recipe
       });
+      swal(
+        'Added to your recipe book!',
+        'We can taste it already. Yum!'
+      );
+    });
   };
 
   render() {
@@ -76,7 +92,7 @@ class RecipeSearchResult extends Component {
           <img src={`https://spoonacular.com/recipeImages/${this.state.recipe.id}-240x150.jpg`} width="240" height="150" alt={this.state.recipe.title}></img>
         </div>
         <Button bsStyle="primary" active bsSize="large" onClick={this.addToRecipeBook.bind(this)}>Add To Recipe Book</Button>
-        <Button bsStyle="info" active bsSize="large" onClick={this.displayFullRecipe.bind(this)}>Full Recipe</Button>
+        <Button bsStyle="primary" active bsSize="large" onClick={this.displayFullRecipe.bind(this)}>Full Recipe</Button>
       </div>
     )
   }
