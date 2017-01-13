cde4993d3cf9373331e419c63c9c100580682a4a
Andrew <chung.andrew7@gmail.com>
2017-01-12 23:54:31 -0500

    testing database

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('planeats', 'postgres', 'sorry1', {
+var db = new Sequelize('planeats', 'postgres', 'thesisEats', {
   dialect: 'postgres'
 });
 
@@ -29,49 +29,31 @@ var UsersEvents = db.define('UsersEvents');
 var MealsRecipes = db.define('MealsRecipes');
 var UsersMeals = db.define('UsersMeals');
 
-// Associations
-//Recipe.sync();
-//Users.sync()
-//  .then(() => {
-//    Users.belongsToMany(Recipe, {through: UsersRecipes, foreignkey: "MealID"});
-//    Recipe.belongsToMany(Users, {through: UsersRecipes, foreignkey: "UserId"});
-//    UsersRecipes.sync();
-//    Meals.sync();
-//  })
-//  .then(() => {
-//    Meals.belongsToMany(Users, {through: UsersMeals, foreignkey: 'MealId'});
-//    Users.belongsToMany(Meals, {through: UsersMeals, foreignkey: 'UserId'});
-//    Recipe.belongsToMany(Meals, {through: MealsRecipes, foreignkey: 'RecipeId'});
-//    Meals.belongsToMany(Recipe, {through: MealsRecipes, foreignkey: 'MealId'});
-//    Events.belongsTo(Meals, {foreignkey: 'MealId'});
-//    Meals.hasMany(Events, {foreignkey: 'MealId'});
-//    UsersMeals.sync();
-//    MealsRecipes.sync();
-//    Events.sync();
-//  })
-//  .then(() => {
-//    Events.belongsToMany(Users, {through: UsersEvents, foreignkey: 'EventId'});
-//    Users.belongsToMany(Events, {through: UsersEvents, foreignkey: 'UserId'});
-//    UsersEvents.sync();
-//  });
-Recipe.sync().then(Users.sync())
-.then(()=>Users.belongsToMany(Recipe,{through:UsersRecipes,foreignkey:"MealID"}))
-.then(()=>Recipe.belongsToMany(Users,{through:UsersRecipes,foreignkey:"UserId"}))
-.then(()=>UsersRecipes.sync())
-.then(()=>Meals.sync())
-.then(()=>Meals.belongsToMany(Users,{through:UsersMeals,foreignkey:'MealId'}))
-.then(()=>Users.belongsToMany(Meals,{through:UsersMeals,foreignkey:'UserId'}))
-.then(()=>Recipe.belongsToMany(Meals,{through:MealsRecipes,foreignkey:'RecipeId'}))
-.then(()=>Meals.belongsToMany(Recipe,{through:MealsRecipes,foreignkey:'MealId'}))
-.then(()=>Events.belongsTo(Meals,{foreignkey:'MealId'}))
-.then(()=>Meals.hasMany(Events,{foreignkey:'MealId'}))
-.then(()=>UsersMeals.sync())
-.then(()=>MealsRecipes.sync())
-.then(()=>Events.sync())
-.then(()=>Events.belongsToMany(Users,{through:UsersEvents,foreignkey:'EventId'}))
-.then(()=>Users.belongsToMany(Events,{through:UsersEvents,foreignkey:'UserId'}))
-.then(()=>UsersEvents.sync())
-.catch((error)=>{console.log(error)});
+ Associations
+Recipe.sync();
+Users.sync()
+  .then(() => {
+    Users.belongsToMany(Recipe, {through: UsersRecipes, foreignkey: "MealID"});
+    Recipe.belongsToMany(Users, {through: UsersRecipes, foreignkey: "UserId"});
+    UsersRecipes.sync();
+    Meals.sync();
+  })
+  .then(() => {
+    Meals.belongsToMany(Users, {through: UsersMeals, foreignkey: 'MealId'});
+    Users.belongsToMany(Meals, {through: UsersMeals, foreignkey: 'UserId'});
+    Recipe.belongsToMany(Meals, {through: MealsRecipes, foreignkey: 'RecipeId'});
+    Meals.belongsToMany(Recipe, {through: MealsRecipes, foreignkey: 'MealId'});
+    Events.belongsTo(Meals, {foreignkey: 'MealId'});
+    Meals.hasMany(Events, {foreignkey: 'MealId'});
+    UsersMeals.sync();
+    MealsRecipes.sync();
+    Events.sync();
+  })
+  .then(() => {
+    Events.belongsToMany(Users, {through: UsersEvents, foreignkey: 'EventId'});
+    Users.belongsToMany(Events, {through: UsersEvents, foreignkey: 'UserId'});
+    UsersEvents.sync();
+  });
 // on delete cascade - when user is deleted it deletes all relations
 
 exports.Recipe = Recipe;
