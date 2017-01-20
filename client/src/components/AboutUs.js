a6e94baf66617ac3aeed2aa82721be1ef9483896
Jordan <mason.jordanp@gmail.com>
2017-01-19 21:30:10 -0600

    Final modifications to delivery button and About Us page

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -20,10 +20,14 @@ class AboutUs extends Component {
 
       <div>
         <h1>About Us</h1>
-        <p>Welcome to PlanEats, a platform for all your meal planning needs. PlanEats helps you plan your meals and keep track of your recipes.
-          For starters we suggest you visit our 'Search' page to search for recipes that you can add to your recipe book or you can create one yourself on the recipe page. You may then create your meals using those recipes.
-          You may then schedule your meals using our calendar platform on the 'Plan' page and populate your shopping list based on those meals on our 'Shop'. When your all ready to cook,
-          you may go to the 'Cook' page which will provide you with ingredients and the cooking instructions for your next meal.
+        <p>Welcome to PlanEats, a platform for all your meal-planning needs. PlanEats helps you plan your meals and keep track of your recipes.
+        </p>
+        <p>
+          For starters, we suggest you visit our <strong>Search</strong> page, where you can search for recipes to add to your recipe book, or you can create one yourself on our <strong>Recipes</strong> page. Once you've added recipes to your recipe book, you can create meals with those recipes on the <strong>Meals</strong> page.
+        </p>
+        <p>
+          You can then schedule your meals using our calendar platform on the <strong>Plan</strong> page and populate your shopping list based on those meals on the <strong>Shop</strong> page. When you're all ready to cook,
+          you can go to the <strong>Cook</strong> page which will provide you with ingredients and the cooking instructions for your next meal.
 
         </p>
       </div>
